dc923f8fb72e54e4c5ff8b793f7751513a86f927
dmitriibo <alienshooter2008@yandex.ru>
2020-09-23 17:53:34 +0300

    fix(global): замена ссылки на компонент дерево

--- src/redux-store/activities/actions.ts
+++ src/redux-store/activities/actions.ts
@@ -58,6 +58,7 @@ const fetchActivitiesList = () => async (dispatch: any) => {
           name: activity.name,
           id: activity.vid,
           iconId: 'blue-line',
+          nodeList: [],
         });
       });
 
