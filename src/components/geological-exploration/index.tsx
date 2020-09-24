0a9e645e660cad530b074f461c52d600974e2d60
dmitriibo <alienshooter2008@yandex.ru>
2020-09-24 13:55:47 +0300

    fix(global): правки по ревью, добавление Layout

--- src/components/geological-exploration/index.tsx
+++ src/components/geological-exploration/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { GridState, GridUpdate, Layout, LayoutWidget } from '@gpn-prototypes/vega-layout';
+import { LayoutWidgetsOverrides } from '@gpn-prototypes/vega-layout/dist/src/LayoutDataView';
 
 import { ActivitiesWidget } from '../activities';
+
+const widgets: LayoutWidget[] = [{ name: 'Мероприятия', component: 'vega-activities' }];
+const widgetsOverrides: LayoutWidgetsOverrides = {
+  'vega-activities': () => <ActivitiesWidget />,
+};
+
+const state: GridState = {
+  0: {
+    type: 'branch',
+    data: {
+      splitDirection: 'down',
+      breakpoint: 15,
+    },
+  },
+  1: {
+    type: 'leaf',
+    data: {
+      widget: 'vega-activities',
+      context: {},
+    },
+  },
+  2: {
+    type: 'leaf',
+    data: {
+      widget: 'vega-activities',
+      context: {},
+    },
+  },
+};
 
 const Container = styled.div`
   width: 100%;
@@ -9,9 +40,18 @@ const Container = styled.div`
 `;
 
 export const GeologicalExploration = (): React.ReactElement => {
+  function action(change: { update: GridUpdate; state: GridState }) {
+    console.log(change);
+  }
+
   return (
     <Container>
-      <ActivitiesWidget />
+      <Layout
+        state={state}
+        widgets={widgets}
+        widgetsOverrides={widgetsOverrides}
+        onChange={action}
+      />
     </Container>
   );
 };
---
+++ src/components/project-structure/icons.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export const OrangeLineSvg = (
+  <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 3.5C0 3.5 3.5 0.5 7.5 0.5C11.5 0.5 12 3.5 12 3.5" stroke="#F38B00" />
+    <path
+      d="M4.5 3.5C0.5 3.5 0 6.5 0 6.5V8.5C1.5 7.66667 3.5 6 7 6C9.5 6 11.1667 7.66667 12 8.5V6.5C12 6.5 8.5 3.5 4.5 3.5Z"
+      fill="white"
+      fillOpacity="0.2"
+    />
+    <path d="M0 10.5C0 10.5 1.5 9.5 5.5 9.5C9.5 9.5 12 10.5 12 10.5" stroke="#F38B00" />
+  </svg>
+);
+
+export const BlueLineSvg = (
+  <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M0 3.5C0 3.5 3.5 0.5 7.5 0.5C11.5 0.5 12 3.5 12 3.5"
+      stroke="white"
+      strokeOpacity="0.2"
+    />
+    <path
+      d="M4.5 3.5C0.5 3.5 0 6.5 0 6.5V8.5C1.5 7.66667 3.5 6 7 6C9.5 6 11.1667 7.66667 12 8.5V6.5C12 6.5 8.5 3.5 4.5 3.5Z"
+      fill="#0AA5FF"
+    />
+    <path
+      d="M0 10.5C0 10.5 1.5 9.5 5.5 9.5C9.5 9.5 12 10.5 12 10.5"
+      stroke="white"
+      strokeOpacity="0.2"
+    />
+  </svg>
+);
+
+export const RedLineSvg = (
+  <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4.5 3.50012C0.5 3.50012 0 6.50012 0 6.50012V3.50012C2 2.33346 5.29928 -0.140019 8.5 0.500124C10.5 0.900124 11.6667 2.33346 12 3.00012V6.50012C12 6.50012 8.5 3.50012 4.5 3.50012Z"
+      fill="#FE4343"
+    />
+    <path
+      d="M0 3.5C0 3.5 3.5 0.5 7.5 0.5C11.5 0.5 12 3.5 12 3.5"
+      stroke="white"
+      strokeOpacity="0.2"
+    />
+    <path
+      d="M4.5 3.5C0.5 3.5 0 6.5 0 6.5V8.5C1.5 7.66667 3.5 6 7 6C9.5 6 11.1667 7.66667 12 8.5V6.5C12 6.5 8.5 3.5 4.5 3.5Z"
+      fill="white"
+      fillOpacity="0.2"
+    />
+    <path
+      d="M0 10.5C0 10.5 1.5 9.5 5.5 9.5C9.5 9.5 12 10.5 12 10.5"
+      stroke="white"
+      strokeOpacity="0.2"
+    />
+  </svg>
+);
