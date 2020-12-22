bee1db7c4d0119cfe4df771f69fe7d4fd976dec7
tumoyakov <leonid.tumoyakov@gmail.com>
2020-12-22 19:30:15 +0700

    fix(components): minor fix
    Jira issue: VEGA-2714

--- src/App/AppView.tsx
+++ src/App/AppView.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useLayoutEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Loader } from '@gpn-prototypes/vega-ui';
 
